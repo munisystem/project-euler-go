d34841c6e2cf59b48adff7512676aa94b4e0f222
munisystem <munisystem@gmail.com>
2016-03-05 22:11:02 +0900

    Fix code format

--- q5.go
+++ q5.go
@@ -4,31 +4,31 @@ package main
 import "fmt"
 
 func main() {
-  mult := 1
-  end := 10
-  for i := 2; i <= end; i++ {
-    if (is_prime(i) == true) {
-      mult = mult * largest_factorial(i, end)
-    }
-  }
-  fmt.Println(mult)
+	mult := 1
+	end := 10
+	for i := 2; i <= end; i++ {
+		if is_prime(i) == true {
+			mult = mult * largest_factorial(i, end)
+		}
+	}
+	fmt.Println(mult)
 }
 
 func is_prime(num int) bool {
-  for i := 2; i <= num / 2 ; i++ {
-    if (num % i == 0) {
-      return false
-    }
-  }
-  return true
+	for i := 2; i <= num/2; i++ {
+		if num%i == 0 {
+			return false
+		}
+	}
+	return true
 }
 
 func largest_factorial(num int, end int) int {
-  fuct := num
-  for ;; {
-    if (fuct * num > end) {
-      return fuct
-    }
-    fuct = fuct * num
-  }
+	fuct := num
+	for {
+		if fuct*num > end {
+			return fuct
+		}
+		fuct = fuct * num
+	}
 }
